8ce233a840f1073cc156381a5369ebc874ecc366
davevassallo@gmail.com <davevassallo@gmail.com>
2021-08-06 08:59:21 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ const qrPreviewSize = {
   margin: 10
 }
 
-function buf2hex(buffer) { // buffer is an ArrayBuffer
+// included this as a debugging tool to help in conversion
+function buf2hex(buffer) { 
   return [...buffer]
       .map(x => x.toString(16).padStart(2, '0'))
       .join('');
@@ -27,13 +28,11 @@ function App() {
   const [qrResult, changeQrResult] = useState()
 
   const handleScan = data => {
-    // data = 'HC1:NCFOXN%TSMAHN-HRL4+/D1BPD9WXU4 E4LR5OGIDLFWKN:X97IIL:PAD6B27 NI4EFSYSG+SB.V Q5-L97K2AL8PZB$UH*%NH$RSC9VFF.C8::HB1M KP8EFH7A9JA5.BODM/AIJVC6LF691R*IN6R%E5BD7JG8CU6O8QGU68ORJSPZHQB27OSP1VPO P6V73EQE46VDPCSOWIRATQ+CR5YOM$P:97N95U/3J$ETG90OARH9P1J4HGZJKCHGX2MTDCZ.COMHW31S3Q:+P*.15-0U5P CO.45HOJ+PB/VSQOL9DLKWCZ3EBKDYGIZ J$XI4OIMEDTJCJKDLEDL9CZTAKBI/8D:8DKTDL+SA057*KB*KYQTKWT4S87WG4KVI9NWGND435:C9COQA584N%4D1Y5ZP6FFBV4LE%T+ZQVLBYF6U7T2AM+9S DVF4WVL63X32 1%S7D372BS5Y4W3IH$G89P/RRV505XALWE'
     if (data) {
 
       let b45EncodedData = data.replace("HC1:", "")      
       let b45DecodedData = base45.decode(b45EncodedData)      
-      let token = pako.inflate(b45DecodedData)
-      
+      let token = pako.inflate(b45DecodedData)     
       
       let outerdata = cbor.decode(token.buffer)[2]
 
